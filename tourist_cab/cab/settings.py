0cfe008a9539173bb7f0d781c90847c01f1da11c
Siva2004 <sivavenkat140@gmail.com>
2026-06-19 23:06:48 +0530

    live page

--- tourist_cab/cab/settings.py
+++ tourist_cab/cab/settings.py
@@ -24,6 +24,8 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
+
+
 # ─────────────────────────────────────────────
 #  APPLICATIONS
 # ─────────────────────────────────────────────
