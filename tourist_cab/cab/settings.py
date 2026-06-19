ee731ddf777a013db46dcf17f85ac50a6b216f05
Siva2004 <sivavenkat140@gmail.com>
2026-06-20 00:42:31 +0530

    fix allowed hosts

--- tourist_cab/cab/settings.py
+++ tourist_cab/cab/settings.py
@@ -21,7 +21,7 @@ if not SECRET_KEY:
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['touristcab.onrender.com', '127.0.0.1', 'localhost']
 
 
 
